30ff3dac3eb9e036cd5ba36c39f672ff1bea4f74
Waverley Leung <waverley.nyc@gmail.com>
2020-03-19 00:29:39 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,12 @@
-def turn_count
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def turn_count(board = "X"))
+  counter = 0
+  board.each do |token|
+    puts board
+
+  counter += 1
 end
 
-def current_player
+def current_player(board)
 end
