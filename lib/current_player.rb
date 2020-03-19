38c81d3fc3f97ebdc5eeccc44236a869e59d30fd
Waverley Leung <waverley.nyc@gmail.com>
2020-03-19 01:51:36 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,7 +3,6 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def turn_count(board = "X")
   counter = 0
   board.each do |board|
-    display_board(board)
     if turn_count(board) == "X" || turn_count(board) == "O"
     counter += 1
   end
